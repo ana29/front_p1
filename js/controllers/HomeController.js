337a6744d3d8e67a5d9112a322fcc7b154492563
Johanny de Lucena Santos <johanny.dls@gmail.com>
2018-09-17 22:11:55 -0300

    Angular funcionando

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -2,11 +2,13 @@ angular.module('condoManager')
 
 .controller('HomeController', function($scope, $http) {
     $scope.nomeApp = 'Condo Manager';
-    $http.get('https://projetop1.herokuapp.com')
+
+    /*$http.get('https://projetop1.herokuapp.com')
         .then(function (data) {
-            $scope.teste = data.body.teste;
+            this.teste = data.body.teste;
         })
         .catch(function (err) {
             console.log(err);
         });
+        */
 });
