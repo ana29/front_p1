803b52b1294805141d5a94f77a9101bdcf2ddbc8
Johanny de Lucena Santos <johanny.dls@gmail.com>
2018-09-20 03:06:58 -0300

    Tela de adicionar morador (sem funcionalidades)

--- app/app.js
+++ app/app.js
@@ -12,30 +12,40 @@ app.config(($routeProvider) => {
         css: [
             'app/components/TelaLogin/Login.css'
         ],
-        controller: 'LoginController'
+        controller: 'LoginController',
+        title: 'Condo Manager - Login'
     })
     .when('/cadastro', {
         templateUrl: 'app/components/TelaCadastro/Cadastro.html',
         css: [
             'app/components/TelaCadastro/Cadastro.css'
         ],
-        controller: 'CadastroController'
+        controller: 'CadastroController',
+        title: 'Condo Manager - Cadastro'
     })
     .when('/home_adm', {
         templateUrl: 'app/components/Admin/TelaHome/HomeAdmin.html',
         css: [
             'app/components/Admin/TelaHome/HomeAdmin.css'
         ],
-        controller: 'HomeAdminController'
+        controller: 'HomeAdminController',
+        title: 'Condo Manager - Home'
     })
     .when('/home_adm/adicionar_morador', {
         templateUrl: 'app/components/Admin/TelaAddMorador/AddMorador.html',
         css: [
             'app/components/Admin/TelaAddMorador/AddMorador.css'
         ],
-        controller: 'AddMoradorController'
+        controller: 'AddMoradorController',
+        title: 'Condo Manager - Administração'
     })
     .otherwise({
         redirectTo: '/login'
     });
-})
+});
+
+app.run(['$rootScope', '$route', function($rootScope, $route) {
+    $rootScope.$on('$routeChangeSuccess', function() {
+        document.title = $route.current.title;
+    });
+}]);
