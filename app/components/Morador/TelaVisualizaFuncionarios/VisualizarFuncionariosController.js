ca188083f8160af993445b79f7cb3f89f25b3d46
liraop <pedrolira@live.com>
2018-10-03 22:03:17 -0300

    Adicionada versõa básica da tela de listagem de funcionários com filtragem

--- app/components/Morador/TelaVisualizaFuncionarios/VisualizarFuncionariosController.js
+++ app/components/Morador/TelaVisualizaFuncionarios/VisualizarFuncionariosController.js
@@ -1,6 +1,32 @@
-angular.module('condoManager')
+    angular.module('condoManager')
 
 .controller('VisualizarFuncionariosController', function($scope, VisualizarFuncionariosService, $rootScope, $localStorage, $location) {
-    $scope.funcionarios = new VisualizarFuncionariosService();
+    //$scope.funcionarios = new VisualizarFuncionariosService();
+    $scope.funcionarios = [];
+
+    let buscaFuncionarios = () => {
+        // query Faz um get no recurso api/staff
+        VisualizarFuncionariosService.query((funcionarios) => {
+            $scope.funcionarios = funcionarios;
+            $scope.mensagem = {};
+        },
+
+            //Callback de erro
+            (erro) => {
+                console.log("Não foi possível obter a lista de serviços");
+                console.error(erro);
+
+                $scope.mensagem = {
+                    texto: 'Não foi possível obter a lista de serviços'
+                };
+            });
+    };
+
+    buscaFuncionarios();
+
+    $scope.selecionaFuncionario = (funcionario) => {
+        $scope.selecionado = funcionario;
+};
+
         
 });
