92b4645abd0406cfeaa380d423e79ea20986f258
Dayana <dayana.silva@ccc.ufcg.edu.br>
2018-10-11 16:19:41 -0300

    alterando controller

--- app/components/Admin/TelaAddLocal/AddLocalController.js
+++ app/components/Admin/TelaAddLocal/AddLocalController.js
@@ -2,4 +2,24 @@ angular.module('condoManager')
 
 .controller('AddLocalController', function($scope, AddLocalService, $localStorage) {
 
+ $scope.local = new AddLocalService();
+
+    $scope.cadastrar = () => {
+        
+        $scope.local.$save()
+
+        .then(() => {
+            if ($scope.frm.$valid) {
+                $scope.mensagem = { texto: "Local adicionado com sucesso" };
+                console.log("Local adicionado com sucesso");
+            }
+
+          
+        })
+
+        .catch((erro) => {
+            $scope.mensagem = { texto: "Não foi possível salvar!" };
+            console.log(erro);
+        });
+    }
 });
