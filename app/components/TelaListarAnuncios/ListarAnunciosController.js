0e7ccd458badf89b6f81e2a853be309de6eeede1
Johanny de Lucena Santos <johanny.dls@gmail.com>
2018-10-17 02:29:45 -0300

    Tela para listar anuncios finalizada

--- app/components/TelaListarAnuncios/ListarAnunciosController.js
+++ app/components/TelaListarAnuncios/ListarAnunciosController.js
@@ -2,4 +2,38 @@ angular.module('condoManager')
 
 .controller('ListarAnunciosController', function($scope, ListarAnunciosService, $localStorage) {
 
+    $scope.anuncios = [];
+    $scope.selecionado = null;
+
+    let buscaAnuncios = () => {
+        // query Faz um get no recurso api/staff
+        let pesquisa = null;
+
+        if ($localStorage.usuarioLogado.condominium_cnpj) {
+            pesquisa = $localStorage.usuarioLogado.condominium_cnpj;
+        } else {
+            pesquisa = $localStorage.usuarioLogado.cnpj;
+        }
+
+        ListarAnunciosService.query({cnpj: pesquisa}, (anuncios) => {
+            $scope.anuncios = anuncios;
+            $scope.mensagem = {};
+        },
+
+            //Callback de erro
+            (erro) => {
+                console.log("Não foi possível obter a lista de anúncios");
+                console.error(erro);
+
+                $scope.mensagem = {
+                    texto: 'Não foi possível obter a lista de anúncios'
+                };
+            });
+    };
+
+    buscaAnuncios();
+
+    $scope.selecionaAnuncio = (anuncio) => {
+        $scope.selecionado = anuncio;
+    };
 });
