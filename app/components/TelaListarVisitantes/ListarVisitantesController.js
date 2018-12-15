620610dec4557ba7c17252093d8bab61e168cb9d
Dayana <dayana.silva@ccc.ufcg.edu.br>
2018-12-15 09:18:15 -0300

    atualizacoes relatorio de teste

--- app/components/TelaListarVisitantes/ListarVisitantesController.js
+++ app/components/TelaListarVisitantes/ListarVisitantesController.js
@@ -1,5 +1,28 @@
 angular.module('condoManager')
 
-.controller('ListarVisitantesController', function($scope, ListarVisitantesService, $localStorage) {
+.controller('ListarVisitantesController', function ($scope, ListarVisitanteService, $localStorage) {
 
+    $scope.visitante = [];
+    $scope.filtro = '';
+    $scope.mensagem = { texto: '' };
+
+
+    let listarVisitante = () => {
+        
+        ListarVisitanteService.query({cnpj: $localStorage.usuarioLogado.cnpj},(visitante) => {
+            $scope.visitante = visitante;
+            $scope.mensagem = {};
+        },
+        
+        (erro) => {
+            console.log("Não foi possível obter a lista de visitantes");
+            console.log(erro);
+
+            $scope.mensagem = {
+                texto: 'Não foi possível obter a lista de visitantes'
+            };
+        });
+    };
+
+    listarVisitante();
 });
