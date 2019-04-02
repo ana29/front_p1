a6617fba5ed7f24d580e67c85b307078f7df68a4
Johanny LS <johanny.dls@gmail.com>
2019-04-01 21:39:27 -0300

    Add anuncio direto da home - Admin, Listar anuncios na home de morador

--- app/components/Admin/TelaHome/HomeAdminController.js
+++ app/components/Admin/TelaHome/HomeAdminController.js
@@ -1,11 +1,36 @@
 angular.module('condoManager')
 
-.controller('HomeAdminController', function($scope, $rootScope, $localStorage) {
+.controller('HomeAdminController', function($scope, $rootScope, $localStorage, AddAnuncioService) {
     $scope.nomeApp = 'Condo Manager';
     $rootScope.usuarioLogado = $localStorage.usuarioLogado;
 
     $rootScope.logout = () => {
         delete $rootScope.usuarioLogado;
         delete $localStorage.usuarioLogado;
+    }
+
+    $scope.anuncio = new AddAnuncioService();
+
+    $scope.anuncio.cnpj = $localStorage.usuarioLogado.cnpj;
+
+    $scope.cadastrarAnuncio = () => {
+        
+        $scope.anuncio.$save()
+
+        .then(() => {
+            if ($scope.frm.$valid) {
+                $scope.mensagem = { texto: "Salvo com sucesso" };
+                console.log("Anúncio adicionado com sucesso!");
+            }
+
+            $scope.anuncio.announcement = '';
+
+        })
+
+        .catch((erro) => {
+            alert("Não foi possível salvar");
+            $scope.mensagem = { texto: "Não foi possível salvar!" };
+            console.log(erro);
+        });
     }
 });
--- app/components/Morador/TelaHome/HomeMoradorController.js
+++ app/components/Morador/TelaHome/HomeMoradorController.js
@@ -1,13 +1,41 @@
 angular.module('condoManager')
 
-.controller('HomeMoradorController', function($scope, $rootScope, $localStorage, $location) {
-    
+.controller('HomeMoradorController', function($scope, $http, $rootScope, $localStorage, $location, $resource, ListarAnunciosService)  {
+
     $rootScope.usuarioLogado = $localStorage.usuarioLogado;
 
+    $scope.anuncios = [];
+    $scope.selecionado = null;
+    
     $rootScope.logout = () => {
         delete $rootScope.usuarioLogado;
         delete $localStorage.usuarioLogado;
         $location.path("/login");
     }
+
+    let buscaAnuncios = () => {
+        // query Faz um get no recurso api/staff
+
+        ListarAnunciosService.query({cnpj: $localStorage.usuarioLogado.cnpj}, (anuncios) => {
+            $scope.anuncios = anuncios;
+            $scope.mensagem = {};
+        },
+
+            //Callback de erro
+            (erro) => {
+                console.log("Não foi possível obter a lista de anúncios");
+                console.error(erro);
+
+                $scope.mensagem = {
+                    texto: 'Não foi possível obter a lista de anúncios'
+                };
+            });
+    };
+
+    buscaAnuncios();
+
+    $scope.selecionaAnuncio = (anuncio) => {
+        $scope.selecionado = anuncio;
+    };
 
 });
